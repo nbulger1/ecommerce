63d4ea2ff7ec77b4aff7f1c80e8eb93a2f705eae
Natalie Bulger <bulgern20@gmail.com>
2022-05-21 14:04:55 -0700

    Fixing product create route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -50,7 +50,7 @@ router.post("/", async (req, res) => {
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
+      if (req.body.tagIds & req.body.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
             product_id: product.id,
@@ -104,7 +104,7 @@ router.put("/:id", async (req, res) => {
         ProductTag.bulkCreate(newProductTags),
       ]);
     })
-    .then((updatedProductTags) => res.json(updatedProductTags))
+    .then((updatedProductTags) => res.status(200).json(updatedProductTags))
     .catch((err) => {
       // console.log(err);
       res.status(400).json(err);
